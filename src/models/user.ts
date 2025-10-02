2d7962c9c4256686a15e88cf9d018ece0b3663e9
SHUBHAM GIRI <shubhamgiriofficial03@gmail.com>
2025-10-03 02:34:36 +0530

    userSchema fixed

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,15 +1,17 @@
 import mongoose, { Model } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { string } from "zod";
+import { config, string } from "zod";
+import config form "../"
+
 
 interface User {
-    username : "string",
-    password : "string"
+    username : string,
+    password : string
 }
 
 interface UserMethod {
-    generateAuthTokens(): "string",
+    generateAuthTokens(): string,
     matchPassword(password : string) : Promise<Boolean>
 }
 
@@ -20,14 +22,23 @@ interface UserModelType extends Model<User, {}, UserMethod>{
 //Making a userSchema
 const userSchema  = new mongoose.Schema<User, UserMethod, UserModelType>({
     username: {
-        type : string,
+        type : String,
         required: true,
         unique: true,
-        minLength: [3, "Minimum length of the username should be 3"]
-        
-
+        minLength: [3, "Minimum length of the username should be 3"],
+        maxLength: [12, "Maximum length of the username cannot exceed 12"]
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+        select: false,
+        minLength: [6, "Minimum length of the password should be 6"],
+        maxLength: [12, "Maximum  length of the password cannot exceed 12"]
     }
 })
 
-
+userSchema.methods.generateAuthTokens = function () {
+    return jwt.sign({_id : this._id},config.JWT_SECRET);
+}
 
