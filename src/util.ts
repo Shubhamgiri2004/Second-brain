69d4159400f551316eb303b2a123a4eb6dec8f62
SHUBHAM GIRI <shubhamgiriofficial03@gmail.com>
2025-10-07 01:44:48 +0530

    utils created

---
+++ src/util.ts
@@ -0,0 +1,8 @@
+export function generateRandomString(length: number){
+    const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = '';
+    for(let i = 0; i<length; i++){
+        result += char.charAt(Math.floor(Math.random() * char.length));
+    }
+    return result;
+}
