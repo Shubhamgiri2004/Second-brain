e1924ae25af3c4fc9e44f3ce648a4c630f6e0b2f
SHUBHAM GIRI <shubhamgiriofficial03@gmail.com>
2025-10-07 02:05:16 +0530

    logic implemented

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import { statusCodes } from "./constants/statusCodes";
+
 export function generateRandomString(length: number){
     const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let result = '';
@@ -5,4 +7,27 @@ export function generateRandomString(length: number){
         result += char.charAt(Math.floor(Math.random() * char.length));
     }
     return result;
+}
+
+function getYoutubeVideoId(ur: string): string | null {
+    if(!url.includes("youtu")) return null;
+    try{
+        const parsedUrl = new URL(url);
+
+        //for short url
+        if(parsedUrl.hostname === "youtu.be") {
+            return parsedUrl.pathname.slice(1); 
+        }
+
+        // standard watch link
+        if(parsedUrl.hostname.includes("youtube.com")){
+            const vParam = parsedUrl.searchParams.get("v");
+            if(vParam) return vParam;
+            
+            return parsedUrl.pathname.s
+        }
+
+    } catch(error){
+       return null;
+    }
 }
