cefd7a680617ac801807f441f684fa7d1af75359
SHUBHAM GIRI <shubhamgiriofficial03@gmail.com>
2025-10-16 00:58:58 +0530

    ThemeDetector completed

--- frontend/src/hooks/useThemeDetector.tsx
+++ frontend/src/hooks/useThemeDetector.tsx
@@ -1,3 +1,20 @@
 import { useState, useEffect } from "react";
 
 // Theme detector
+const useThemeDetector = ()=>{
+    const getCurrentTheme =  ()=> window.matchMedia("(prefer-color-theme : dark)").matches;
+    const [isDarkTheme, setIsDarkTheme] = useState(getCurrentTheme());
+    
+    const mqListener = ((e : any)=>{
+        setIsDarkTheme(e.matches);
+    });
+
+    useEffect(()=>{
+        const darkThemeMq = window.matchMedia("(prefer-color-theme: dark)").matches;
+        darkThemeMq.addListener(mqListener);
+        return () => darkThemeMq.removeListener(mqListener);
+    },[]);
+    return isDarkTheme;
+}
+
+export default useThemeDetector;
