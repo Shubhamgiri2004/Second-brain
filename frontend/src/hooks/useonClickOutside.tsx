708290b2e167071d0065f2ad5ae353693bfda3d5
SHUBHAM GIRI <shubhamgiriofficial03@gmail.com>
2025-10-16 23:40:03 +0530

    ClickOutside hook created

---
+++ frontend/src/hooks/useonClickOutside.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+
+function useOnCLickOutSide( ref : any, handler : (event: any)=>void) {
+    useEffect(()=> {
+        const Listener = (event : any)=> {
+
+            // do noting if the ref element or descendent element
+            if(!ref.current || ref.current.contains(event.target)) {
+                return;
+            }
+
+            //calling the event if it is outside the element
+            handler(event);
+        };
+
+        //blinding the event listener
+        document.addEventListener("mousedown", Listener);
+        document.addEventListener("touchstart", Listener);
+
+        // now cleaning up the event listener on unmount
+        return ()=>{
+            document.addEventListener("mousedown", Listener);
+            document.addEventListener("touchstart", Listener);
+        };
+    }, [ref, handler]);
+}
+
+export default useOnCLickOutSide;
