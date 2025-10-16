256a558b5a66b7ab3463ee5b02c95be9ea18a1fc
SHUBHAM GIRI <shubhamgiriofficial03@gmail.com>
2025-10-17 01:39:46 +0530

    singUp logic completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-
-
+import SignUp from "./pages/SignUp";
 import './App.css'
+import { Route, Routes} from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
+    <div className='dark:bg-slate-900 h-screen'>
+      <Routes>
+        <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
+    </div>
   )
+  
 }
 
 export default App
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,15 +1,41 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
+import useDarkMode from "../hooks/useDarkMode";
 
 
-const SignUp = () =>{
+const SignUp = () => {
     const navigate = useNavigate();
-    const [userName, setUsername] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [msg, setMsg] = useState("");
-    const [isDarkMode, toggleDarkMode]  = useDarkMode("");
+    const [setDarkMode, toggleDarkMode] = useDarkMode();
 
-    
+    //writing an event happen 
+    const onSubmitHandler = async (e: any) => {
+        e.preventDefault();
+
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/signup`, {
+            username, password
+        });
+
+        try {
+            if (response.status === 200) {
+                localStorage.setItem("token", response.data.token);
+                navigate("/dashboard");
+                setUsername("");
+                setPassword("");
+            }
+        } catch (error: unknown) {
+            if (axios.isAxiosError(error)) {
+                setMsg(error.response?.data?.msg || "something went wrong");
+            } else {
+                setMsg("Network error. Please refresh again.")
+            }
+        }
+    }
+
 
 }
+
+export default SignUp;
